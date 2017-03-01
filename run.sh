7ef2d3ce822a17d3f66ae0ce7fbc4563e7b2c256
Ivan Zhang <ivanzhangsolutions@gmail.com>
2017-02-28 20:20:12 -0500

    hard linking latest scrape to `latest.json` (#1)
    * make latest.json file
    
    * Add new scrape
    
    * make name into variable
    
    * oops
    
    * wat
    
    * i am bash noob
    
    * Add new scrape
    
    * Add new scrape
    
    * revert temporarily
    
    * Add new scrape
    
    * talk to me
    
    * Add new scrape
    
    * Add new scrape
    
    * delete empty files
    
    * shhh
    
    * Add new scrape
    
    * Add new scrape
    
    * delete generated
    
    * Add new scrape
    
    * hard link
    
    * Add new scrape
    
    * remove generated files

--- run.sh
+++ run.sh
@@ -5,7 +5,12 @@ SECTION='S,Y'
 [ -d $ACADEM_SESSION ] || mkdir $ACADEM_SESSION
 
 git pull --rebase
-curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$(date +%s).json
+
+SCRAPENAME=$(date +%s).json
+
+curl --silent "https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=${SECTION}&studyyear=&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title=" > $ACADEM_SESSION/$SCRAPENAME 
+
+ln $ACADEM_SESSION/$SCRAPENAME $ACADEM_SESSION/latest.json
 
 git add "${ACADEM_SESSION}/"
 git commit -m "Add new scrape"
